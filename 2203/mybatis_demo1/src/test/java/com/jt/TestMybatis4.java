947ab62ae96e0802d6bf93c5c19a053eaf0501be
WOW-QAQ <8003434+wow-qaq@user.noreply.gitee.com>
2022-03-31 17:19:00 +0800

    一对多映射

--- 2203/mybatis_demo1/src/test/java/com/jt/TestMybatis4.java
+++ 2203/mybatis_demo1/src/test/java/com/jt/TestMybatis4.java
@@ -2,6 +2,7 @@ package com.jt;
 
 import com.jt.mapper.DeptMapper;
 import com.jt.mapper.EmpMapper;
+import com.jt.pojo.Dept;
 import com.jt.pojo.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,13 @@ public class TestMybatis4 {
     @Test
     public void TestOneToOne(){
         List<Emp> list = empMapper.oneToOne();
+        System.out.println(list);
+    }
+    //部门和员工一对多
+    @Test
+    public void testOneToMore(){
+
+        List<Dept> list = deptMapper.findAll();
         System.out.println(list);
     }
 }
