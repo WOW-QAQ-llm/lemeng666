fb70ac83f7b4db280311644dc2381de08571e6b1
WOW-QAQ <8003434+wow-qaq@user.noreply.gitee.com>
2022-04-02 09:29:37 +0800

    左侧列表的实现

---
+++ 2203/jt/src/main/java/com/jt/mapper/RightsMapper.java
@@ -0,0 +1,11 @@
+package com.jt.mapper;
+
+import com.jt.pojo.Rights;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface RightsMapper {
+    @Select("select * from rights where parent_id=#{parentId}")
+    List<Rights> findByParentId(int parentId);
+}
